6efbc90781806cfb0e6b3b2eb2eb79bff6f68a86
jacob-myers <jakemyers190@gmail.com>
2025-02-07 00:33:11 -0500

    Adding RomParser, static class that can convert from romaji to hirigana.

---
+++ lib/functions/RomParser.dart
@@ -0,0 +1,131 @@
+import 'package:collection/collection.dart';
+
+const HEPBURNTAB_H = """ぁ      xa      あ      a      ぃ      xi      い      i      ぅ      xu
+う      u      う゛      vu      う゛ぁ      va      う゛ぃ      vi      う゛ぇ      ve
+う゛ぉ      vo      ぇ      xe      え      e      ぉ      xo      お      o
+
+
+か      ka      が      ga      き      ki      きゃ      kya      きゅ      kyu
+きょ      kyo      ぎ      gi      ぎゃ      gya      ぎゅ      gyu      ぎょ      gyo
+く      ku      ぐ      gu      け      ke      げ      ge      こ      ko
+ご      go
+
+さ      sa      ざ      za      し      shi      しゃ      sha      しゅ      shu
+しょ      sho      じ      ji      じゃ      ja      じゅ      ju      じょ      jo
+す      su      ず      zu      せ      se      ぜ      ze      そ      so
+ぞ      zo
+
+た      ta      だ      da      ち      chi      ちゃ      cha      ちゅ      chu
+ちょ      cho      ぢ      di      ぢゃ      dya      ぢゅ      dyu      ぢょ      dyo
+
+っ      xtsu
+っう゛      vvu      っう゛ぁ      vva      っう゛ぃ      vvi
+っう゛ぇ      vve      っう゛ぉ      vvo
+っか      kka      っが      gga      っき      kki      っきゃ      kkya
+っきゅ      kkyu      っきょ      kkyo      っぎ      ggi      っぎゃ      ggya
+っぎゅ      ggyu      っぎょ      ggyo      っく      kku      っぐ      ggu
+っけ      kke      っげ      gge      っこ      kko      っご      ggo      っさ      ssa
+っざ      zza      っし      sshi      っしゃ      ssha
+っしゅ      sshu      っしょ      ssho
+っじ      jji      っじゃ      jja      っじゅ      jju      っじょ      jjo
+っす      ssu      っず      zzu      っせ      sse      っぜ      zze      っそ      sso
+っぞ      zzo      った      tta      っだ      dda      っち      cchi
+っちゃ      ccha      っちゅ      cchu      っちょ      ccho      っぢ      ddi
+っぢゃ      ddya      っぢゅ      ddyu      っぢょ      ddyo      っつ      ttsu
+っづ      ddu      って      tte      っで      dde      っと      tto      っど      ddo
+っは      hha      っば      bba      っぱ      ppa      っひ      hhi
+っひゃ      hhya      っひゅ      hhyu      っひょ      hhyo      っび      bbi
+っびゃ      bbya      っびゅ      bbyu      っびょ      bbyo      っぴ      ppi
+っぴゃ      ppya      っぴゅ      ppyu      っぴょ      ppyo      っふ      ffu
+っふぁ      ffa      っふぃ      ffi      っふぇ      ffe      っふぉ      ffo
+っぶ      bbu      っぷ      ppu      っへ      hhe      っべ      bbe      っぺ      ppe
+っほ      hho      っぼ      bbo      っぽ      ppo      っや      yya      っゆ      yyu
+っよ      yyo      っら      rra      っり      rri      っりゃ      rrya
+っりゅ      rryu      っりょ      rryo      っる      rru      っれ      rre
+っろ      rro
+
+つ      tsu      づ      du      て      te      で      de      と      to
+ど      do
+
+な      na      に      ni      にゃ      nya      にゅ      nyu      にょ      nyo
+ぬ      nu      ね      ne      の      no
+
+は      ha      ば      ba      ぱ      pa      ひ      hi      ひゃ      hya
+ひゅ      hyu      ひょ      hyo      び      bi      びゃ      bya      びゅ      byu
+びょ      byo      ぴ      pi      ぴゃ      pya      ぴゅ      pyu      ぴょ      pyo
+ふ      fu      ふぁ      fa      ふぃ      fi      ふぇ      fe      ふぉ      fo
+ぶ      bu      ぷ      pu      へ      he      べ      be      ぺ      pe
+ほ      ho      ぼ      bo      ぽ      po
+
+ま      ma      み      mi      みゃ      mya      みゅ      myu      みょ      myo
+む      mu      め      me      も      mo
+
+ゃ      xya      や      ya      ゅ      xyu      ゆ      yu      ょ      xyo
+よ      yo
+
+ら      ra      り      ri      りゃ      rya      りゅ      ryu      りょ      ryo
+る      ru      れ      re      ろ      ro
+
+ゎ      xwa      わ      wa      ゐ      wi      ゑ      we
+を      wo      ん      n
+
+ん     n'
+でぃ   dyi
+ー     -
+ちぇ    che
+っちぇ      cche
+じぇ      je
+""";
+
+class RomParser {
+  static final Map<String, String> _hepburnHR = __hepburn(true);
+  static final Map<String, String> _hepburnRH = __hepburn(false);
+
+  static final _ROMPAT_H = __ROMPAT_H();
+
+  static RegExp __ROMPAT_H() {
+    var keys = _hepburnRH.keys.toList();
+    keys.sort((a, b) => b.length.compareTo(a.length));
+    return RegExp(keys.join("|"));
+  }
+
+  static Map<String, String> __hepburn(bool HR) {
+    /// Map hirigana characters and romaji representations.
+    /// input: HR - If true, maps hirigana to romaji, else maps romaji to hirigana.
+
+    // Eliminates whitespace and puts all characters in list.
+    var lst = HEPBURNTAB_H.split(RegExp(r"\s+"));
+
+    // Unzips the list into romaji and hirigana.
+    List<String> r = [], h = [];
+    for (int i = 0; i < lst.length; i++) {
+      if (i % 2 == 0) {
+        h.add(lst[i]);
+      } else {
+        r.add(lst[i]);
+      }
+    }
+    h.removeLast(); // Has an empty item.
+
+    if (HR) { // Maps hirigana to romaji.
+      return Map<String, String>.fromIterables(h, r);
+    } else { // Maps romaji to hirigana.
+      return Map<String, String>.fromIterables(r, h);
+    }
+  }
+
+  static String toHirigana(String roma) {
+    /// Convert given romaji to hirigana characters.
+    roma = roma.toLowerCase();
+
+    String hiri = roma.replaceAllMapped(_ROMPAT_H, (match) {
+      return _hepburnRH[match.group(0)] ?? '';
+    });
+
+    return hiri;
+  }
+}
+
+void main() {
+  print(RomParser.toHirigana("kokkoii"));
+}
