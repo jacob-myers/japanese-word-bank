b2f95079f1661b020934db825a64d07a855a49a4
jacob-myers <jakemyers190@gmail.com>
2025-02-08 02:45:24 -0500

    Adding a romaji to katakana function to RomParser.

--- lib/functions/RomParser.dart
+++ lib/functions/RomParser.dart
@@ -77,19 +77,107 @@ const HEPBURNTAB_H = """ぁ      xa      あ      a      ぃ      xi      い   
 じぇ      je
 """;
 
+const HEPBURNTAB = """ァ      xa      ア       a       ィ       xi      イ       i       ゥ       xu
+ウ       u       ヴ       vu      ヴァ      va      ヴィ      vi      ヴェ      ve
+ヴォ      vo      ェ       xe      エ       e       ォ       xo      オ       o
+
+
+カ       ka      ガ       ga      キ       ki      キャ      kya     キュ      kyu
+キョ      kyo     ギ       gi      ギャ      gya     ギュ      gyu     ギョ      gyo
+ク       ku      グ       gu      ケ       ke      ゲ       ge      コ       ko
+ゴ       go
+
+サ       sa      ザ       za      シ       shi     シャ      sha     シュ      shu
+ショ      sho     シェ    she
+ジ       ji      ジャ      ja      ジュ      ju      ジョ      jo
+ス       su      ズ       zu      セ       se      ゼ       ze      ソ       so
+ゾ       zo
+
+タ       ta      ダ       da      チ       chi     チャ      cha     チュ      chu
+チョ      cho     ヂ       di      ヂャ      dya     ヂュ      dyu     ヂョ      dyo
+ティ    ti
+
+ッ       xtsu
+ッヴ      vvu     ッヴァ     vva     ッヴィ     vvi
+ッヴェ     vve     ッヴォ     vvo
+ッカ      kka     ッガ      gga     ッキ      kki     ッキャ     kkya
+ッキュ     kkyu    ッキョ     kkyo    ッギ      ggi     ッギャ     ggya
+ッギュ     ggyu    ッギョ     ggyo    ック      kku     ッグ      ggu
+ッケ      kke     ッゲ      gge     ッコ      kko     ッゴ      ggo     ッサ      ssa
+ッザ      zza     ッシ      sshi    ッシャ     ssha
+ッシュ     sshu    ッショ     ssho    ッシェ  sshe
+ッジ      jji     ッジャ     jja     ッジュ     jju     ッジョ     jjo
+ッス      ssu     ッズ      zzu     ッセ      sse     ッゼ      zze     ッソ      sso
+ッゾ      zzo     ッタ      tta     ッダ      dda     ッチ      cchi    ッティ  tti
+ッチャ     ccha    ッチュ     cchu    ッチョ     ccho    ッヂ      ddi
+ッヂャ     ddya    ッヂュ     ddyu    ッヂョ     ddyo    ッツ      ttsu
+ッヅ      ddu     ッテ      tte     ッデ      dde     ット      tto     ッド      ddo
+ッドゥ  ddu
+ッハ      hha     ッバ      bba     ッパ      ppa     ッヒ      hhi
+ッヒャ     hhya    ッヒュ     hhyu    ッヒョ     hhyo    ッビ      bbi
+ッビャ     bbya    ッビュ     bbyu    ッビョ     bbyo    ッピ      ppi
+ッピャ     ppya    ッピュ     ppyu    ッピョ     ppyo    ッフ      ffu     ッフュ  ffu
+ッファ     ffa     ッフィ     ffi     ッフェ     ffe     ッフォ     ffo
+ッブ      bbu     ップ      ppu     ッヘ      hhe     ッベ      bbe     ッペ      ppe
+ッホ      hho     ッボ      bbo     ッポ      ppo     ッヤ      yya     ッユ      yyu
+ッヨ      yyo     ッラ      rra     ッリ      rri     ッリャ     rrya
+ッリュ     rryu    ッリョ     rryo    ッル      rru     ッレ      rre
+ッロ      rro
+
+ツ       tsu     ヅ       du      テ       te      デ       de      ト       to
+ド       do      ドゥ    du
+
+ナ       na      ニ       ni      ニャ      nya     ニュ      nyu     ニョ      nyo
+ヌ       nu      ネ       ne      ノ       no
+
+ハ       ha      バ       ba      パ       pa      ヒ       hi      ヒャ      hya
+ヒュ      hyu     ヒョ      hyo     ビ       bi      ビャ      bya     ビュ      byu
+ビョ      byo     ピ       pi      ピャ      pya     ピュ      pyu     ピョ      pyo
+フ       fu      ファ      fa      フィ      fi      フェ      fe      フォ      fo
+フュ    fu
+ブ       bu      プ       pu      ヘ       he      ベ       be      ペ       pe
+ホ       ho      ボ       bo      ポ       po
+
+マ       ma      ミ       mi      ミャ      mya     ミュ      myu     ミョ      myo
+ム       mu      メ       me      モ       mo
+
+ャ       xya     ヤ       ya      ュ       xyu     ユ       yu      ョ       xyo
+ヨ       yo
+
+ラ       ra      リ       ri      リャ      rya     リュ      ryu     リョ      ryo
+ル       ru      レ       re      ロ       ro
+
+ヮ       xwa     ワ       wa      ウィ    wi      ヰ wi      ヱ       we      ウェ    we
+ヲ       wo      ウォ    wo      ン n
+
+ン     n'
+ディ   di
+ー     -
+チェ    che
+ッチェ     cche
+ジェ      je
+""";
+
 class RomParser {
-  static final Map<String, String> _hepburnHR = __hepburn(true);
-  static final Map<String, String> _hepburnRH = __hepburn(false);
+  static final Map<String, String> _hepburnHR = __hepburnH(true);
+  static final Map<String, String> _hepburnRH = __hepburnH(false);
+  static final Map<String, String> _hepburnRK = __hepburnK(false);
 
   static final _ROMPAT_H = __ROMPAT_H();
-
   static RegExp __ROMPAT_H() {
     var keys = _hepburnRH.keys.toList();
     keys.sort((a, b) => b.length.compareTo(a.length));
     return RegExp(keys.join("|"));
   }
 
-  static Map<String, String> __hepburn(bool HR) {
+  static final _ROMPAT_K = __ROMPAT_K();
+  static RegExp __ROMPAT_K() {
+    var keys = _hepburnRK.keys.toList();
+    keys.sort((a, b) => b.length.compareTo(a.length));
+    return RegExp(keys.join("|"));
+  }
+
+  static Map<String, String> __hepburnH(bool HR) {
     /// Map hirigana characters and romaji representations.
     /// input: HR - If true, maps hirigana to romaji, else maps romaji to hirigana.
 
@@ -114,6 +202,31 @@ class RomParser {
     }
   }
 
+  static Map<String, String> __hepburnK(bool KR) {
+    /// Map katakana characters and romaji representations.
+    /// input: HR - If true, maps katakana to romaji, else maps romaji to katakana.
+
+    // Eliminates whitespace and puts all characters in list.
+    var lst = HEPBURNTAB.split(RegExp(r"\s+"));
+
+    // Unzips the list into romaji and katakana.
+    List<String> r = [], k = [];
+    for (int i = 0; i < lst.length; i++) {
+      if (i % 2 == 0) {
+        k.add(lst[i]);
+      } else {
+        r.add(lst[i]);
+      }
+    }
+    k.removeLast(); // Has an empty item.
+
+    if (KR) { // Maps katakana to romaji.
+      return Map<String, String>.fromIterables(k, r);
+    } else { // Maps romaji to katakana.
+      return Map<String, String>.fromIterables(r, k);
+    }
+  }
+
   static String toHirigana(String roma) {
     /// Convert given romaji to hirigana characters.
     roma = roma.toLowerCase();
@@ -124,8 +237,20 @@ class RomParser {
 
     return hiri;
   }
+
+  static String toKatakana(String roma) {
+    /// Convert given romaji to katakana characters.
+    roma = roma.toLowerCase();
+
+    String kata = roma.replaceAllMapped(_ROMPAT_K, (match) {
+      return _hepburnRK[match.group(0)] ?? '';
+    });
+
+    return kata;
+  }
 }
 
 void main() {
   print(RomParser.toHirigana("kokkoii"));
+  print(RomParser.toKatakana("kokako-ra"));
 }
